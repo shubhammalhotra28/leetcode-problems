019747e96350581d3c39ab536defca727e7d1e84
shubhammalhotra28 <56790284+shubhammalhotra28@users.noreply.github.com>
2020-04-20 18:47:23 -0400

    TwoSum using one iteration

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,3 +1,29 @@
+// one iteration using hashmap
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> v;
+        for(int i = 0;i<nums.size();i++)
+        {
+            int diff = target-nums[i];
+            if(v.count(diff)) // finds if v.count(diff) is true
+            {
+                vector<int> ans;
+                ans.push_back(v[diff]);
+                ans.push_back(i);
+                return ans;
+            }
+            v[nums[i]] = i;
+        }
+       return {}; 
+    }
+};
+
+
+
+
+// Brute force approach
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
