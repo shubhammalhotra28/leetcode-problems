9df153113fa68cf91fd76914d2347953f2db564e
shubhammalhotra28 <56790284+shubhammalhotra28@users.noreply.github.com>
2020-04-20 17:22:04 -0400

    Container With Most Water using brute force

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -19,3 +19,34 @@ class Solution {
         return max;
     }
 }
+
+// using one iteration now
+
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+        int max = Integer.MIN_VALUE;
+        int i = 0;
+        int j = height.length-1;
+        while(i<j)
+        {
+            int min = Math.min(height[i],height[j]);
+            max = Math.max(max,(j-i)*min);
+            
+            if(height[i]<height[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return max;
+     
+    }
+}
+
+
+
