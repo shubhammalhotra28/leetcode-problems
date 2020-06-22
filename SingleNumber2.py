8a4cf811e031a3497b81feb4e5d61307af8f90ff
shubham <shubham.malhotra20@gmail.com>
2020-06-22 17:31:20 -0400

    Single Number 2 - leetcode 137

---
+++ SingleNumber2.py
@@ -0,0 +1,34 @@
+# using o(n) space
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] in dict:
+                dict[nums[i]]+=1
+            else:
+                dict[nums[i]] = 1
+        ans = -1000
+        for i in range(len(nums)):
+            if dict[nums[i]]==1:
+                ans = nums[i]
+        return ans
+
+
+using O(1) space :
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        
+        seen_once = 0
+        seen_twice = 0
+        
+        for num in nums:
+            
+            seen_once = ~seen_twice&(seen_once^num)
+            seen_twice = ~seen_once&(seen_twice^num)
+            
+        return seen_once
+            
